14be535731f4b570f6efc2860d64646345783d89
liupeng110 <717219917@qq.com>
2017-05-31 14:54:35 +0800

    Initial commit

---
+++ app/src/main/java/com/happ/happ/activity/Activity_Main.kt
@@ -0,0 +1,28 @@
+package com.happ.happ.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.happ.happ.R
+import kotlinx.android.synthetic.main.activity_main.*
+import org.greenrobot.eventbus.EventBus
+
+class Activity_Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_tv.text="kotlin"
+
+
+
+        fun onClick(){
+             var a =0;
+            EventBus.getDefault().postSticky("标签2"+a)
+
+        }
+
+
+
+    }
+}
